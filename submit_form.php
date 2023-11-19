cf40ca405b2e4644547093926e5c595a14e4ea96
u8312 <upathak@northislandcollege.ca>
2023-11-18 20:20:49 -0800

    completed media queries for tab

--- submit_form.php
+++ submit_form.php
@@ -1,2 +1,2 @@
 Your information has been received.
-. Thank you for your queries.We will get back to you within 3 business days.
+ Thank you for your queries.We will get back to you within 3 business days.
